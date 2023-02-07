d9719438acff5f1cec99b5cd9e1efcede0dda299
ziemieu <ziemieu86@gmail.com>
2023-02-07 15:36:59 +0100

    Render Dep

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,6 @@
 const express = require ("express");
 const route = express.Router();
-const data = require("../data/data.json")
+// const data = require("../data/data.json")
 
 route.get('/', (req, res )=>{
     res.send("Hello world")
