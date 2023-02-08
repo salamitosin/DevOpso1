512e526d0c87b21db9022aaaab1edab6606e2bf9
ziemieu <ziemieu86@gmail.com>
2023-02-08 12:28:10 +0100

    Render Dep

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,27 @@
 const express = require ("express");
 const route = express.Router();
-// const data = require("../data/data.json")
+const data = require("../data/data.json");
+const{check, validator, validationResult} = require('express-validator')
 
 route.get('/', (req, res )=>{
-    res.send("Hello world")
+    res.json(data)
+});
+
+// 
+route.post('/', 
+[
+    check("name", "app name is required").not().isEmpty(),
+    check("author", "app name is required").not().isEmpty()
+],
+(req, res )=>{
+   
+    const errors = validationResult(req);
+    
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
 });
 
 module.exports = route;
--- test/app.test.js
+++ test/app.test.js
@@ -15,19 +15,43 @@ app.get('/', (req, res) =>{
 app.use('/api', appRoute);
 
 describe("Integration function for our api's", ()=>{
+
+    // Intergrated test for Get APi
     it("GET /api -success - get all the dataset ", async ()=>{
         const {body, statusCode} = await request (app).get('/api');
 
-        // expect(body).toEqual(
-        //     expect.arrayContaining([
-        //         expect.objectContaining({
-        //             id: expect.any(Number),
-        //             name: expect.any(String),
-        //             author: expect.any(String)
-        //         })
-        //     ])
-        // );
+        expect(body).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    id: expect.any(Number),
+                    name: expect.any(String),
+                    author: expect.any(String)
+                })
+            ])
+        );
         // expect(body).toBe("Hello world");
         expect(statusCode).toBe(200);
     });
+
+    //Intergrated test for Post APi
+    it('Post /api  Success or Failure', async ()=>{
+        const {body, statusCode} = await request(app).post('/api').send({
+            name: "",
+            author: "Paul Smith"
+        });
+
+        expect(statusCode).toBe(400);
+        console.log(body)
+        expect(body).toEqual(    {
+            errors: [
+                {
+                value: '',
+                msg: 'app name is required',
+                param: 'name',
+                location: 'body'
+                }
+            ]
+    });
+
+    })
 });
